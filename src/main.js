b4b4d74b469a7e8f21740b22cdd9368c6a0a3036
Yoni Yomtov <jonathan.yomtov@whitesourcesoftware.com>
2019-10-14 16:21:59 -0400

    no message

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const utilities = require('./utilities');
 const exec = require('@actions/exec');
-
+const fs = require('fs');
 
 async function run() {
 	try {
@@ -86,109 +86,33 @@ async function run() {
 		await exec.exec('java', uaVars);
 		
 		// Get the location of the scan log file
-		let result = '';
-		const options = {listeners: {}};
-		options.listeners.stdout = function(data) {
-			result += data.toString();
-			console.log('result: ' + result);
-		};
-        await exec.exec('ls', ['-alF']);
-        await exec.exec('ls ./whitesource/', ['-alF']);
-		await exec.exec('find', ['./whitesource/', '-name', '\*scan_report.json'], options);
-		
-		// Set the output parameters
-        result = result.replace(/(\r\n|\n|\r)/gm,"").trim();
-        core.info('Scan log file: ' + result);
-		core.setOutput('scan-report-file-path', result);
-		const folder = result.substr(0, result.lastIndexOf('/'));
-		core.setOutput('scan-report-folder-path', folder);
+        let logFilePath = '';
+        const logFileFolder = './whitesource/';
+        let files = fs.readdirSync(logFileFolder);
+        for(let i =0; i < files.length; i++) {
+            if (files[i].endsWith('scan_report.json')) {
+                logFilePath = logFileFolder + files[i];
+                break;
+            }
+        }
 
-		await exec.exec('cat', [result]);
+		// Set the output parameters
+		core.setOutput('scan-report-file-path', result);
+		core.setOutput('scan-report-folder-path', logFileFolder);
+
+		// Print the log file if needed
+        let isPrintScanReport = core.getInput('print-scan-report');
+        if (isPrintScanReport === 'true') {
+            core.info('print scan true');
+            core.info('path: ' + logFilePath);
+             let scanReport = fs.readFileSync(logFilePath);
+             core.info('Scan report:\n' + JSON.stringify(scanReport));
+         } else {
+             core.info('print scan false');
+         }
 	} catch (error) {
 		core.setFailed(error.message);
 	}
 }
 
 run();
-
-// var scanPath = '';
-// download('https://github.com/whitesource/unified-agent-distribution/releases/latest/download/wss-unified-agent.jar', "wss-unified-agent.jar", function (err) {
-// utilities.download('https://wss-qa.s3.amazonaws.com/unified-agent/integration/wss-unified-agent-integration-763.jar', "wss-unified-agent.jar", function (err) {
-    // try {
-        // var dockerVersion = utilities.execShellCommand('docker -v');
-        // dockerVersion.then(
-            // result => {
-                // core.info('Docker version is ' + result);
-                // return utilities.execShellCommand('ls -alF');
-                // // return utilities.execShellCommand('docker rmi $(docker images -a -q)');
-            // }
-        // ).then(
-            // result => {
-                // core.info('ls command output \n' + result);
-                // const gprToken = core.getInput('gpr-token');
-                // return utilities.execShellCommand('docker login docker.pkg.github.com -u whitesource-yossi -p ' + gprToken);
-            // }
-        // ).then(
-            // result => {
-                // core.info('Docker login result ' + result);
-                // return utilities.execShellCommand('docker pull docker.pkg.github.com/whitesource-yossi/githubactiontesting2/localdjango:1.0');
-            // }
-        // ).then(
-            // result => {
-                // core.info('Docker pull results ' + result);
-                // return utilities.execShellCommand('docker images');
-            // }
-        // ).then(
-            // result => {
-                // core.info('Docker images results ' + result);
-                // const destinationUrl = core.getInput('ws-destination-url');
-                // const wsApiKey = core.getInput('ws-api-key');
-                // const wsUserKey = core.getInput('ws-user-key');
-                // const wsProjectKey = core.getInput('ws-project-key');
-                // return utilities.execShellCommand('java -jar wss-unified-agent.jar -d . -wss.url "' + destinationUrl + '" -apiKey ' + wsApiKey + ' -projectToken ' + wsProjectKey + ' -noConfig true -generateScanReport true -userKey ' + wsUserKey);
-                // // return utilities.execShellCommand('java -jar wss-unified-agent.jar -d . -wss.url "' + destinationUrl + '" -apiKey ' + wsApiKey + ' -projectToken ' + wsProjectKey + ' -noConfig true -"docker.scanImages" true -generateScanReport true -userKey ' + wsUserKey);
-            // }
-        // ).then(
-            // result => {
-                // core.info('UA run results \n' + result);
-                // // return utilities.execShellCommand('find "$(pwd -P)" -name "*scan_report.json"')
-                // return utilities.execShellCommand('find -name "*scan_report.json"')
-                // // return utilities.execShellCommand('find /home/runner/work/GitHubActionTesting2/GitHubActionTesting2/./whitesource/ -name "*scan_report.json"');
-            // }
-        // ).then(
-            // result => {
-                // scanPath = result;
-                // core.info('Scan report path: ' + scanPath);
-                // return utilities.execShellCommand('ls -alF ./whitesource');
-            // }
-        // ).then(
-            // result => {
-                // core.info('Second ls result \n' + result);
-
-                // core.setOutput('scan-report-file-path', scanPath);
-                // var n = scanPath.lastIndexOf('/');
-                // var folder = scanPath.substr(0, n);
-                // core.setOutput('scan-report-folder-path', folder);
-
-                // let scanReport = fs.readFileSync(scanPath);
-                // core.info('Scan report:\n' + JSON.stringify(scanReport));
-
-                // // let isPrintScanReport = core.getInput('print-scan-report');
-                // // if (isPrintScanReport === 'true') {
-                // //     core.info('path: ' + result);
-                // //     core.info('print scan true');
-                // //     let scanReport = fs.readFileSync(scanPath);
-                // //     core.info('Scan report:\n' + JSON.stringify(scanReport));
-                // //     // return utilities.execShellCommand('cat "' + result +'"');
-                // // } else {
-                // //     core.info('print scan false');
-                // // }
-                // return {};
-            // }
-        // ).catch(err => {
-            // core.error('Exception \n' + err);
-        // });
-    // } catch (error) {
-        // core.setFailed(error.message);
-    // }
-// });
